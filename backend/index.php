af41f429bb0dfeaf4c582038eee2ff5e34d095fc
Alexey-2077 <alekseybelyakov004@gmail.com>
2025-05-17 16:42:46 +0000

    Вернул основной код в index.php

--- backend/index.php
+++ backend/index.php
@@ -1,8 +1,18 @@
 <?php
-// Включаем отображение всех ошибок
+// Включаем ошибки
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+?>
 
-echo "PHP работает!";
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>Добро пожаловать</title>
+</head>
+<body>
+  <h1>Это мой сайт!</h1>
+</body>
+</html>
 
